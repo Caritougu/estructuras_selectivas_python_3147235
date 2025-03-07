d5c5bfec221a3011b996940ae49f83035b112c8d
Angel <angeldrc1612@gmail.com>
2025-03-07 10:58:40 -0500

    If simple

---
+++ if_simple.py
@@ -0,0 +1,21 @@
+"""
+Sintaxis if simple:
+If condicional : 
+    Intruccion1
+    Intruccion2
+    Intruccion3
+    Intruccion1
+
+
+
+"""
+
+#Ejemplo
+
+a=9
+b=3
+if a > b :
+     print("a es mayor que b")
+     print("y hoy es viernes")
+     
+     
